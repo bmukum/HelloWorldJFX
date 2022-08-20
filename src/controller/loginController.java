c9e6ad15e4b845e3b23695b90a78fe2835eb51fa
bmukum <bmukum99@gmail.com>
2022-08-20 16:18:16 -0600

    login screen working

---
+++ src/controller/loginController.java
@@ -0,0 +1,46 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.ZoneId;
+import java.util.ResourceBundle;
+
+public class loginController implements Initializable {
+    public Label location;
+    public TextField usernameTF;
+    public TextField passwordTF;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        ZoneId zoneId = ZoneId.systemDefault();
+        location.setText(zoneId.getId());
+
+    }
+
+    public void Login(ActionEvent actionEvent) throws IOException {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/main.fxml"));
+            Parent root = loader.load();
+
+            Stage addPartStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            Scene addPartScene = new Scene(root, 1300, 690);
+            addPartStage.setTitle("Main Screen");
+            addPartStage.setScene(addPartScene);
+            addPartStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/model/Login.java
+++ src/model/Login.java
@@ -1,26 +1,29 @@
-package sample;
+package model;
 
+import database.JDBC;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.time.ZoneId;
 import java.util.Locale;
 
-public class Main extends Application {
+public class Login extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/login.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         JDBC.makeConnection();
+        ZoneId zoneId = ZoneId.systemDefault();
         Locale.setDefault(new Locale("fr"));
         launch(args);
     }
