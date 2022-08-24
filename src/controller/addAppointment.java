6e9505456600d32fae0ce3a095f1b4f2c04c28a8
bmukum <bmukum99@gmail.com>
2022-08-23 20:18:52 -0600

    time check good to go

--- src/controller/addAppointment.java
+++ src/controller/addAppointment.java
@@ -1,5 +1,6 @@
 package controller;
 
+import database.DBAppointments;
 import database.DBContacts;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -8,10 +9,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.Appointments;
 import model.Contacts;
 
 import java.net.URL;
@@ -20,6 +23,7 @@ import java.sql.Timestamp;
 import java.time.*;
 import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.TimeZone;
 
 public class addAppointment implements Initializable {
     public TextField idTF;
@@ -58,7 +62,43 @@ public class addAppointment implements Initializable {
             LocalTime endLocalTime = LocalTime.parse(endTF.getText());
             LocalDateTime localStart = LocalDateTime.of(localDate, startLocalTime);
             LocalDateTime localEnd = LocalDateTime.of(localDate, endLocalTime);
-            String start = startTF.getText();
+
+            LocalTime openEST = LocalTime.of(8,00);
+            LocalTime closeEST = LocalTime.of(22,00);
+            ZoneId localZoneId = ZoneId.of(TimeZone.getDefault().getID());
+
+            ZoneId estZone = ZoneId.of("America/New_York");
+            ZonedDateTime estOpenHour = ZonedDateTime.of(localDate, openEST, estZone);
+            ZonedDateTime estCloseHour = ZonedDateTime.of(localDate, closeEST, estZone);
+
+            ZonedDateTime estOpenToLocal = estOpenHour.withZoneSameInstant(localZoneId);
+            ZonedDateTime estCloseToLocal = estCloseHour.withZoneSameInstant(localZoneId);
+
+            if (startLocalTime.isBefore(estOpenToLocal.toLocalTime())) {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Warning Dialog");
+                alert.setContentText("Please enter a time after 8:00 am EST.");
+                alert.showAndWait();
+                return;
+            }
+
+            if (endLocalTime.isAfter(estCloseToLocal.toLocalTime())) {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Warning Dialog");
+                alert.setContentText("Please enter a time before 10:00 pm EST.");
+                alert.showAndWait();
+                return;
+            }
+
+            int rowsAdded = DBAppointments.
+
+//            ZonedDateTime startZoned = localStart.atZone(ZoneId.of(ZoneId.systemDefault().toString()));
+//            ZonedDateTime startZonedEst = startZoned.withZoneSameInstant(ZoneId.of("America/New_York"));
+//
+//            ZonedDateTime endZoned = localEnd.atZone(ZoneId.of(ZoneId.systemDefault().toString()));
+//            ZonedDateTime endZonedEst = endZoned.withZoneSameInstant(ZoneId.of("America/New_York"));
+
+
 
         } catch (Exception e) {
             e.printStackTrace();
